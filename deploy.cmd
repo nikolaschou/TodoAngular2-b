dc87b1d4c91cc9bca1181e7edfe1398e406b7b4e
Nikola <nikolaschou@gmail.com>
2016-01-04 16:10:34 +0100

    Adding call to cmd-file

--- deploy.cmd
+++ deploy.cmd
@@ -1,7 +1,7 @@
 dir
 cd
-npm install gulp
-gulp
+call npm install gulp
+call gulp
 echo %DEPLOYMENT_SOURCE% and %DEPLOYMENT_TARGET%
 dir build /S /R
 xcopy %DEPLOYMENT_SOURCE%/build %DEPLOYMENT_TARGET% /Y
